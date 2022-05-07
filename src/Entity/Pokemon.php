87650dbb0d5d4bb017aa466e03dfadd1e2b8ea5a
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-05-07 12:24:34 +0200

    validator

--- src/Entity/Pokemon.php
+++ src/Entity/Pokemon.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\PokemonRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PokemonRepository::class)]
 class Pokemon
@@ -20,6 +21,7 @@ class Pokemon
     private $decription;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Assert\Url]
     private $image;
 
     #[ORM\Column(type: 'integer', nullable: true)]
